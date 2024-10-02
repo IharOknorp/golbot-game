0f443abfdea4fba3b7e0e321b595ea1ed6811064
ihar_pronka <ihar_pronka@external.mckinsey.com>
2024-10-02 16:25:00 +0200

    Первый коммит

---
+++ main.js
@@ -0,0 +1,145 @@
+// Определяем класс сцены до конфигурации игры
+class GameScene extends Phaser.Scene {
+  constructor() {
+    super({ key: 'GameScene' });
+    this.score = 0;
+    this.goalSpeed = 100;
+    this.ballSpeed = 500;
+    this.previousScore = 0;
+  }
+
+  preload() {
+    // Загружаем изображения
+    this.load.image('player', 'assets/player.png');
+    this.load.image('ball', 'assets/ball.png');
+    this.load.image('goal', 'assets/goal.png');
+    // Загрузка звуков (опционально)
+    // this.load.audio('shootSound', 'assets/shoot.mp3');
+    // this.load.audio('goalSound', 'assets/goal.mp3');
+  }
+
+  create() {
+    // Добавляем ворота как динамический объект
+    this.goal = this.physics.add.image(700, 300, 'goal'); // 700 = 800 - 100
+    this.goal.setScale(1.5).refreshBody();
+    this.goal.setImmovable(true); // Ворота не будут двигаться при столкновениях
+
+    // Добавляем игрока
+    this.player = this.physics.add.sprite(100, 300, 'player');
+    this.player.setCollideWorldBounds(true);
+
+    // Добавляем мяч
+    this.ball = this.physics.add.sprite(this.player.x + 50, this.player.y, 'ball');
+    this.ball.setCollideWorldBounds(true);
+    this.ball.setBounce(1, 1);
+    this.ball.setVelocity(0, 0);
+
+    // Обработка столкновений мяча с воротами
+    this.physics.add.overlap(this.ball, this.goal, this.scoreGoal, null, this);
+
+    // Добавляем текст для счета
+    this.scoreText = this.add.text(10, 10, 'Голов: 0', { fontSize: '24px', fill: '#ffffff' });
+
+    // Добавляем кнопку для удара
+    this.shootButton = this.add.text(325, 550, 'Удар', {
+      fontSize: '32px',
+      fill: '#00ff00',
+      backgroundColor: '#000000',
+      padding: { x: 10, y: 5 },
+      borderRadius: 5
+    })
+      .setInteractive()
+      .on('pointerdown', () => {
+        this.shootBall();
+      });
+
+    // Добавляем клавиатурное управление (опционально)
+    this.cursors = this.input.keyboard.createCursorKeys();
+  }
+
+  update() {
+    // Автоматическое движение ворот вверх и вниз
+    if (this.goal.y < 100) {
+      this.goal.setVelocityY(this.goalSpeed);
+    } else if (this.goal.y > 500) { // 600 - 100
+      this.goal.setVelocityY(-this.goalSpeed);
+    }
+
+    // Если ворота не движутся, начинаем движение
+    if (this.goal.body.velocity.y === 0) {
+      this.goal.setVelocityY(this.goalSpeed);
+    }
+
+    // Логика увеличения сложности
+    if (this.score > 0 && this.score % 5 === 0 && this.score !== this.previousScore) {
+      this.goalSpeed += 20;
+      this.ballSpeed += 50;
+      this.previousScore = this.score;
+    }
+
+    // Дополнительная логика (например, управление игроком с клавиатуры)
+    /*
+    this.player.setVelocity(0);
+
+    if (this.cursors.left.isDown) {
+        this.player.setVelocityX(-200);
+    }
+    else if (this.cursors.right.isDown) {
+        this.player.setVelocityX(200);
+    }
+
+    if (this.cursors.up.isDown) {
+        this.player.setVelocityY(-200);
+    }
+    else if (this.cursors.down.isDown) {
+        this.player.setVelocityY(200);
+    }
+
+    // Обновление позиции мяча, если он не в движении
+    if (this.ball.body.velocity.x === 0 && this.ball.body.velocity.y === 0) {
+        this.ball.setPosition(this.player.x + 50, this.player.y);
+    }
+    */
+  }
+
+  shootBall() {
+    // Устанавливаем начальную позицию мяча рядом с игроком
+    this.ball.setPosition(this.player.x + 50, this.player.y);
+    // Задаем скорость мяча в направлении ворот
+    this.physics.moveToObject(this.ball, this.goal, this.ballSpeed);
+    // Воспроизведение звука удара (опционально)
+    // this.sound.play('shootSound');
+  }
+
+  scoreGoal(ball, goal) {
+    // Увеличиваем счет
+    this.score += 1;
+    this.scoreText.setText('Голов: ' + this.score);
+
+    // Перемещаем мяч обратно к игроку после забитого гола
+    this.ball.setPosition(this.player.x + 50, this.player.y);
+    this.ball.setVelocity(0, 0);
+
+    // Воспроизведение звука гола (опционально)
+    // this.sound.play('goalSound');
+  }
+}
+
+// Основная конфигурация игры (должна быть после определения GameScene)
+const config = {
+  type: Phaser.AUTO,
+  width: 800,
+  height: 600,
+  backgroundColor: '#2d2d2d',
+  physics: {
+    default: 'arcade',
+    arcade: {
+      gravity: { y: 0 },
+      debug: false
+    }
+  },
+  scene: [GameScene] // Используем класс GameScene
+};
+
+// Создаём новый экземпляр игры
+const game = new Phaser.Game(config);
